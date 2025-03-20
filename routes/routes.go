df08986a43f285561b5519278d4df26e4a08e2d2
fieldchaser <fieldchaser1@gmail.com>
2025-03-20 11:00:25 +0800

    更新浏览器所需资源文件名称不同问题

--- routes/routes.go
+++ routes/routes.go
@@ -2,10 +2,10 @@ package routes
 
 import (
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"net/http"
-	"github.com/gin-contrib/pprof"
 	"time"
 	"web_framework/controllers"
 	_ "web_framework/docs" // 千万不要忘了导入把你上一步生成的docs
@@ -38,12 +38,18 @@ func Setup() *gin.Engine {
 		MaxAge:           12 * time.Hour,
 	}))
 
+	r.LoadHTMLFiles("templates/index.html")
+	r.Static("/static", "./static")
+	r.Static("/css", "./static/css")
+	r.Static("/js", "./static/js")
 	v1 := r.Group("/api/v1")
 	//前端发来的http请求对应的路由操作
 	//1.注册界面
 	v1.POST("/signup", controllers.SignUp)
 	v1.POST("/login", controllers.Login)
 
+	v1.GET("/posts2", controllers.GetPostListHandlers2)
+	v1.GET("/posts", controllers.GetPostListHandlers)
 	v1.Use(middlewares.JWTAuthMiddleware())
 
 	{
@@ -52,15 +58,12 @@ func Setup() *gin.Engine {
 
 		v1.POST("/post", controllers.CreatePostHandlers)
 		v1.GET("/post/:id", controllers.GetPostDetailHandlers)
-		v1.GET("/posts", controllers.GetPostListHandlers)
-
-		v1.GET("/posts2", controllers.GetPostListHandlers2)
 
 		v1.POST("/vote", controllers.PostVoteHandlers)
 	}
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "ok")
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
 	pprof.Register(r) // 添加pprof来对程序进行性能调优
